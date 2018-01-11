86b0bde9465afea8e93bebb1195a6b5161d3e78a
oprachun <osstapp@gmail.com>
2018-01-11 23:48:51 +0200

    a

--- vikings.js
+++ vikings.js
@@ -1,156 +1,217 @@
 'use strict';
-var things = {
-  'Шлемы': {
-    'Стандартные вещи': [
+function HTMLTable(ATableHE, AMRP, APNAlignToRight)
+{
+  function Init()
+  {
+    var
+      LInitValue = JSON.parse(localStorage[LLSIdent] || '{}'),
+      LNF = window.Intl ? new Intl.NumberFormat('en-US') : null;
+    LTRs  = Array.prototype.slice.call(ATableHE.getElementsByTagName('tr'), 0);
+    LTRTH = null;
+    LParentHE = null;
+    for(var i = LTRs.length - 1; i >= 0; i--)
+      if((LTRs[i].parentNode.tagName === 'THEAD') || (LTRs[i].parentNode.tagName === 'TFOOT'))
+        LTRs.shift();
+      else
+      if(LTRs[i].children[0].tagName === 'TH')
       {
-        name: 'Древний Шлем',
-        level: 50,
-        percent: {
-          'урон убийц': [9, 13.5, 18, 22.5, 27, 36],
-          'здоровье всех войск': [3, 4.5, 6, 7.5, 9, 12]
-        }
-      },
+        LTRTH = LTRs.shift();
+        LParentHE = LTRTH.parentNode;
+      }
+
+    for(var i = 0, L = LTHs.length, LSortIndex = -1; i < L; i++)
+    {
+      LTHs[i].addEventListener('click', SortCol);
+      LTHs[i].fssIndex = i;
+      if(LTHs[i].textContent === LInitValue.ColName)
       {
-        name: 'Заговоренный Шлем',
-        level: 50,
-        percent: {
-          'здоровье всех войск': [9, 13.5, 18, 22.5, 27, 36],
-          'урон пехоты': [3, 4.5, 6, 7.5, 9, 12]
-        }
-      },
+        LTHs[i].fssDirection = LInitValue.Direction;
+        LSortIndex = i;
+      }
+      else
+        LTHs[i].fssDirection = 1;
+      LTHs[i].style.cursor = 'pointer';
+      LTHs[i].fssIsNumber = true;
+      var LtextContent;
+      for(var j = 0, LValue; j < LTRs.length; j++)
       {
-        name: 'Серебряный Шлем',
-        level: 50,
-        percent: {
-          'урон стрелков': [9, 13.5, 18, 22.5, 27, 36],
-          'защита всех войск': [3, 4.5, 6, 7.5, 9, 12]
+        LtextContent = LTRs[j].children[i].textContent;
+        if(parseFloat(LtextContent) != LtextContent)
+        {
+          LTHs[i].fssIsNumber = false;
+          break;
         }
-      },
-      {
-        name: 'Шлем тирана',
-        level: 50,
-        percent: {
-          'урон кавалерии': [9, 13.5, 18, 22.5, 27, 36],
-          'производство еды': [24, 48, 72, 96, 120, 180]
+      }
+      if(LTHs[i].fssIsNumber)
+        for(var j = 0, LValue; j < LTRs.length; j++)
+        {
+          LTRs[j].children[i].className = 'Number';
+          if(LNF)
+            LTRs[j].children[i].innerHTML = LNF.format(LTRs[j].children[i].innerHTML);
         }
-      },
-      {
-        name: 'Шлем ярла',
-        level: 50,
-        percent: {
-          'производство еды': [48, 96, 144, 192, 240, 360],
-          'урон пехоты': [6, 9, 12, 15, 18, 24]
-        }
-      },
-      {
-        name: 'Шлем Разрушителя',
-        level: 45,
-        percent: {
-          'производство серебра': [11, 22, 33, 44, 55, 82.5],
-          'урон пехоты': [2.8, 4.1, 5.5, 6.9, 8.3, 11],
-          'урон убийц': [5.6, 8.2, 11, 13.8, 16.6, 22]
-        }
-      },
-      {
-        name: 'Шлем Убийцы',
-        level: 45,
-        percent: {
-          'производство еды': [22, 44, 66, 88, 110, 165],
-          'урон пехоты': [2.8, 4.1, 5.5, 6.9, 8.3, 11],
-          'урон осадных войск': [5.6, 8.2, 11, 13.8, 16.6, 22]
-        }
-      },
-      {
-        name: 'Маска Локи',
-        level: 40,
-        percent: {
-          'грузоподъемность отряда': [5, 10, 15, 20, 25, 30],
-          'урон стрелков': [7.5, 11.4, 15, 18.9, 22.5, 30]
-        }
-      },
-      {
-        name: 'Шлем Воителя',
-        level: 35,
-        percent: {
-          'защита всех войск': [4.6,6.8,9,11.2,13.6,18],
-          'здоровье всех войск': [2.3,3.4,4.5,5.6,6.8,9],
-          'урон убийц': [2.3,3.4,4.5,5.6,6.8,9]
-        }
-      }/*,
-      {
-        name: '',
-        level: ,
-        percent: {
-          '': [],
-          '': [],
-          '': [],
-          '': []
-        }
-      },
-      {
-        name: '',
-        level: ,
-        percent: {
-          '': [],
-          '': [],
-          '': [],
-          '': []
-        }
-      },*/
-    ],
-    'Вещи захватчиков': [
-    ],
-    'Специальные вещи': [
-    ]
-  },
-  'Броня': {
-    'Стандартные вещи': [
-    ],
-    'Вещи захватчиков': [
-    ],
-    'Специальные вещи': [
-    ]
-  },
-  'Оружие': {
-    'Стандартные вещи': [
-    ],
-    'Вещи захватчиков': [
-    ],
-    'Специальные вещи': [
-    ]
-  },
-  'Обувь': {
-    'Стандартные вещи': [
-    ],
-    'Вещи захватчиков': [
-    ],
-    'Специальные вещи': [
-    ]
-  },
-  'Амулеты': {
-    'Стандартные вещи': [
-    ],
-    'Вещи захватчиков': [
-    ],
-    'Специальные вещи': [
-    ]
+   }
+   if(LSortIndex !== -1)
+     SortCol({target: LTHs[LSortIndex]});
   }
-};
+
+  function PageNavigatorBuild()
+  {
+    function HECreate(ACaption, APageNo)
+    {
+      var LHE = LPageNavigatorHE.appendChild(document.createElement('span'));
+      LHE.innerHTML = ACaption;
+      LHE.addEventListener('click', function(){ PageNoSet(APageNo); });
+      LHE.style.cursor = 'pointer';
+      LHE.style.padding = '.1em .4em';
+      return LHE;
+    }
+
+    function HEsUpdate(APageNo)
+    {
+      LHEs[LPageNo - 1].style.color = '';
+      LHEs[APageNo - 1].style.color = 'red';
+      LPrevHE.style.color = APageNo === 1 ? 'grey' : '';
+      LNextHE.style.color = APageNo === LCount ? 'grey' : '';
+    }
+
+    function PageNoSet(APageNo)
+    {
+      if(APageNo < 1)
+        APageNo = LPageNo - 1;
+      else
+      if(APageNo > LCount)
+        APageNo = LPageNo + 1;
+      else
+      if(!APageNo)
+        APageNo = LPageNo;
+
+      if((APageNo >= 1) && (APageNo <= LCount))
+      {
+        HEsUpdate(APageNo);
+        LPageNo = APageNo;
+        var LStartIndex = (APageNo - 1) * AMRP;
+        for(var i = 0, L = LTRs.length; i < L; i++)
+          LTRs[i].style.display = ((i >= LStartIndex) && (i < LStartIndex + AMRP) ? '' : 'none');
+      }
+    }
+
+    if((typeof AMRP !== 'number') || (AMRP < 0))
+      AMRP = 0;
+    if((AMRP <= 0) || (LTRs.length <= AMRP))
+      return;
+    var
+      LCount = Math.ceil(LTRs.length / AMRP),
+      LHEs = [],
+      LPageNo = 1,
+      LPageNavigatorHE = ATableHE.parentNode.insertBefore(document.createElement('div'), ATableHE),
+      LPrevHE = HECreate('<', 0);
+    if(APNAlignToRight)
+      LPageNavigatorHE.style.textAlign = 'right';
+    if('ontouchstart' in window)
+      LPageNavigatorHE.style.fontSize = '32px';
+    for(var i = 1; i <= LCount; i++)
+      LHEs.push(HECreate(i, i));
+    var LNextHE = HECreate('>', LCount + 1);
+    PageNoSet(1);
+    return PageNoSet;
+  }
+
+  function SortCol(AEvent)
+  {
+    function Sort(ATR1, ATR2)
+    {
+      function LNum(AValue)
+      {
+        return +AValue.replace(/,/g, '');
+      }
+      var
+        LValue1 = ATR1.children[AEvent.currentTarget.fssIndex].textContent,
+        LValue2 = ATR2.children[AEvent.currentTarget.fssIndex].textContent;
+      if(AEvent.currentTarget.fssIsNumber)
+        return (LNum(LValue1) - LNum(LValue2)) * AEvent.currentTarget.fssDirection;
+      else
+        return LValue1.toLowerCase().localeCompare(LValue2.toLowerCase()) * AEvent.currentTarget.fssDirection;
+    }
+
+    LTRs.sort(Sort);
+    for(var i = 0, L = LTRs.length, LHE = LTRTH, LTR; i < L; i++)
+    {
+      LTR = LTRs[i];
+      LParentHE.insertBefore(LTR, LHE.nextSibling);
+      LHE = LTR;
+    }
+    for(var i = 0, L = LTHs.length; i < L; i++)
+      if(AEvent.currentTarget === LTHs[i])
+        LTHs[i].className = (LTHs[i].fssDirection === -1 ? 'ascend' : 'descend');
+      else
+        LTHs[i].className = '';
+     if(LLSIdent)
+      localStorage[LLSIdent] = JSON.stringify({
+        ColName: LTRTH.children[AEvent.currentTarget.fssIndex].textContent,
+        Direction: AEvent.currentTarget.fssDirection
+      });
+    AEvent.currentTarget.fssDirection *= -1;
+    LPageNoSet && LPageNoSet();
+  }
+
+  function TableHEInit()
+  {
+    if(ATableHE.constructor === String)
+      ATableHE = document.getElementById(ATableHE);
+    if(ATableHE.constructor !== HTMLTableElement)
+      throw new Error('Not html table');
+  }
+
+  TableHEInit();
+  /*if('ontouchstart' in window)
+    ATableHE.style.fontSize = '20px';*/
+  var LTHs = ATableHE.getElementsByTagName('th'), LTRs, LTRTH, LParentHE;
+  if(!LTHs.length)
+    return;
+  var LLSIdent = ATableHE.id ? location.pathname + ATableHE.id : '';
+  Init();
+  var LPageNoSet = PageNavigatorBuild();
+}
+
+(function()
+{
+  function StyleAdd(AName, AStyles)
+  {
+    if(document.styleSheets[0].addRule)
+      document.styleSheets[0].addRule(AName, AStyles);
+    else
+      document.styleSheets[0].insertRule(AName + '{' + AStyles  + '}', 0);
+  }
+  if(!document.styleSheets[0])
+    document.head.appendChild(document.createElement('style'));
+  StyleAdd('th.ascend:after',  'content: "\\2193";');
+  StyleAdd('th.descend:after', 'content: "\\2191";');
+  StyleAdd('.Number', 'text-align: right;');
+  /*if('ontouchstart' in window)
+    StyleAdd('th', 'font-size: 32px;');*/
+})();
 
 function HEsBuild()
 {
-  var LThing, LName, LParams = {}, i;
+  var LThing, LName, LParams = {}, i, LPercents = [];
   for(i = 0; i < Things.length; i++)
   {
     LThing = Things[i];
     for(LName in LThing.percent)
+    {
       LParams[LName] = true;
+      if(LPercents.indexOf(LThing.percent[LName].join()) === -1)
+        LPercents.push(LThing.percent[LName].join()); 
+    }
   }                      
-  LParams = Object.keys(LParams).sort();  
+  LParams = Object.keys(LParams).sort();
+  console.log(LParams.join('\r\n'));
+  LPercents.sort();  
+  //console.log(LPercents.join('\r\n'));  
 
   var LFragHE = document.createDocumentFragment();
   var LTableHE = LFragHE.appendChild(document.createElement('table'));
-  var LTRTHHE = LTableHE.appendChild(document.createElement('thead')).appendChild(document.createElement('tr'));
+  var LTRTHHE = LTableHE.appendChild(document.createElement('tr'));
   var LTHHE; 
   LTRTHHE.appendChild(document.createElement('th')).innerHTML = '<div><span>Название</span></div>'; 
   LTRTHHE.appendChild(document.createElement('th')).innerHTML = '<div><span>Уровень'; 
@@ -175,5 +236,6 @@ function HEsBuild()
     }
   }                      
   document.body.appendChild(LFragHE);
+  HTMLTable(LTableHE);
 }
 HEsBuild();
